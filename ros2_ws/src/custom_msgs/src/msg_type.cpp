b30319eeb782725bb14db46348acc4b93c7c30c1
Anand John <60827309+ajpiano12@users.noreply.github.com>
2023-08-15 14:24:00 -0400

    rewrote return statements

--- ros2_ws/src/custom_msgs/src/msg_type.cpp
+++ ros2_ws/src/custom_msgs/src/msg_type.cpp
@@ -15,21 +15,25 @@
 namespace msg_type{
     frc::Quaternion Quaternion_to_type(custom_msgs::msg::Quaternion& quaternion){
         float w = quaternion.w, i = quaternion.i, j = quaternion.j, k = quaternion.k;
-        return frc::Quaternion(w,i,j,k);
+        frc::Quaternion rt(w,i,j,k);
+        return rt;
     }
 
     frc::Rotation3d Rotation3d_to_type(custom_msgs::msg::Rotation3d& rotation3d){
-        return frc::Rotation3d(Quaternion_to_type(rotation3d.q));
+        frc::Rotation3d rt(Quaternion_to_type(rotation3d.q));
+        return rt;
     }
 
     frc::Translation3d Translation3d_to_type(custom_msgs::msg::Translation3d& translation3d){
         float x = translation3d.x, y = translation3d.y, z = translation3d.z;
-        return frc::Translation3d(x,y,z);
+        frc::Translation3d rt(x,y,z);
+        return rt;
     }
 
     frc::Pose3d Pose3d_to_type(custom_msgs::msg::Pose3d& pose3d){
-        return frc::Pose3d(
+        frc::Pose3d rt(
             Translation3d_to_type(pose3d.t),
             Rotation3d_to_type(pose3d.r));
+        return rt;
     }
 }
