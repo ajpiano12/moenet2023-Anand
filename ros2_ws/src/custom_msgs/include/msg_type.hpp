80942ab125d1c06e412decd15ccc36d6a68e5c6e
Beelink <maltimjohn1@gmail.com>
2023-08-15 13:47:12 -0400

    headers fixed somewhat

--- ros2_ws/src/custom_msgs/include/msg_type.hpp
+++ ros2_ws/src/custom_msgs/include/msg_type.hpp
@@ -1,14 +1,14 @@
 #ifndef __MSG_TYPE_HPP__
 #define __MSG_TYPE_HPP__
 
-#include <frc/geometry/Pose3d.h>
-#include <custom_msgs/msg/pose3d.hpp>
+#include "frc/geometry/Pose3d.h"
+#include "msg/pose3d.hpp"
 
-#include <frc/geometry/Rotation3d.h>
-#include <custom_msgs/msg/rotation3d.hpp>
+#include "frc/geometry/Rotation3d.h"
+#include "custom_msgs/msg/rotation3d.hpp"
 
-#include <frc/geometry/Translation3d.h>
-#include <custom_msgs/msg/translation3d.hpp>
+#include "frc/geometry/Translation3d.h"
+#include "custom_msgs/msg/translation3d.hpp"
 
 namespace msg_type{
     frc::Pose3d Pose3d_to_type(custom_msgs::msg::Pose3d& pose3d);
--- ros2_ws/src/custom_msgs/src/msg_type.cpp
+++ ros2_ws/src/custom_msgs/src/msg_type.cpp
@@ -1,14 +1,14 @@
-#include "include/msg_type.hpp"
+#include "msg_type.hpp"
 
-#include <frc/geometry/Pose3d.h>
-#include <custom_msgs/msg/pose3d.hpp>
+#include "frc/geometry/Pose3d.h"
+#include "msg/pose3d.hpp"
 
-#include <frc/geometry/Rotation3d.h>
-#include <frc/geometry/Quaternion.h>
-#include <custom_msgs/msg/rotation3d.hpp>
+#include "frc/geometry/Rotation3d.h"
+#include "frc/geometry/Quaternion.h"
+#include "custom_msgs/msg/rotation3d.hpp"
 
-#include <frc/geometry/Translation3d.h>
-#include <custom_msgs/msg/translation3d.hpp>
+#include "frc/geometry/Translation3d.h"
+#include "custom_msgs/msg/translation3d.hpp"
 
 namespace msg_type{
     frc::Pose3d Pose3d_to_type(custom_msgs::msg::Pose3d& pose3d){
