f579b9a1c040a7edb867bd0a1efb2d9669ee454a
Anand John <60827309+ajpiano12@users.noreply.github.com>
2023-08-15 13:22:50 -0400

    Created msg_type function for pose3d

--- ros2_ws/src/custom_msgs/src/msg_type.cpp
+++ ros2_ws/src/custom_msgs/src/msg_type.cpp
@@ -1,5 +1,23 @@
 #include "include/msg_type.hpp"
 
+#include <frc/geometry/Pose3d.h>
+#include <custom_msgs/msg/pose3d.hpp>
+
+#include <frc/geometry/Rotation3d.h>
+#include <frc/geometry/Quaternion.h>
+#include <custom_msgs/msg/rotation3d.hpp>
+
+#include <frc/geometry/Translation3d.h>
+#include <custom_msgs/msg/translation3d.hpp>
+
 namespace msg_type{
-    
-};
+    frc::Pose3d Pose3d_to_type(custom_msgs::msg::Pose3d& pose3d){
+        float x = pose3d.x, y = pose3d.y, z = pose3d.z;
+        float w = pose3d.q.w, i = pose3d.q.i, j = pose3d.q.j, k = pose3d.q.k;
+        return frc::Pose3d::Pose3d(
+            frc::Translation3d::Translation3d(x,y,z),
+            frc::Rotation3d::Rotation3d(
+                frc::Quaternion::Quaternion(w,i,j,k))
+        );
+    }
+}
