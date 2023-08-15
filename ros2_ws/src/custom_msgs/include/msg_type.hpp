a7b50b254ca0ca9feab0e6de6e6ee32d8f6de156
Anand John <60827309+ajpiano12@users.noreply.github.com>
2023-08-15 14:14:06 -0400

    wrote converters for msgs

--- ros2_ws/src/custom_msgs/include/msg_type.hpp
+++ ros2_ws/src/custom_msgs/include/msg_type.hpp
@@ -11,6 +11,12 @@
 #include "custom_msgs/msg/translation3d.hpp"
 
 namespace msg_type{
+    frc::Quaternion Quaternion_to_type(custom_msgs::msg::Quaternion& quaternion);
+
+    frc::Rotation3d Rotation3d_to_type(custom_msgs::msg::Rotation3d& rotation3d);
+
+    frc::Translation3d Translation3d_to_type(custom_msgs::msg::Translation3d& translation3d)
+
     frc::Pose3d Pose3d_to_type(custom_msgs::msg::Pose3d& pose3d);
 }
 
--- ros2_ws/src/custom_msgs/src/msg_type.cpp
+++ ros2_ws/src/custom_msgs/src/msg_type.cpp
@@ -3,21 +3,32 @@
 #include "frc/geometry/Pose3d.h"
 #include "custom_msgs/msg/pose3d.hpp"
 
-#include "frc/geometry/Rotation3d.h"
 #include "frc/geometry/Quaternion.h"
+#include "custom_msgs/msg/quaternion.hpp"
+
+#include "frc/geometry/Rotation3d.h"
 #include "custom_msgs/msg/rotation3d.hpp"
 
 #include "frc/geometry/Translation3d.h"
 #include "custom_msgs/msg/translation3d.hpp"
 
 namespace msg_type{
+    frc::Quaternion Quaternion_to_type(custom_msgs::msg::Quaternion& quaternion){
+        float w = quaternion.w, i = quaternion.i, j = quaternion.j, k = quaternion.k;
+    }
+
+    frc::Rotation3d Rotation3d_to_type(custom_msgs::msg::Rotation3d& rotation3d){
+        return frc::Rotation3d::Rotation3d(Quaternion_to_type(rotation3d.q));
+    }
+
+    frc::Translation3d Translation3d_to_type(custom_msgs::msg::Translation3d& translation3d){
+        float x = translation3d.x, y = translation3d.y, z = translation3d.z;
+        return frc::Translation3d::Translation3d(x,y,z);
+    }
+
     frc::Pose3d Pose3d_to_type(custom_msgs::msg::Pose3d& pose3d){
-        float x = pose3d.x, y = pose3d.y, z = pose3d.z;
-        float w = pose3d.q.w, i = pose3d.q.i, j = pose3d.q.j, k = pose3d.q.k;
         return frc::Pose3d::Pose3d(
-            frc::Translation3d::Translation3d(x,y,z),
-            frc::Rotation3d::Rotation3d(
-                frc::Quaternion::Quaternion(w,i,j,k))
-        );
+            Translation3d_to_type(pose3d.t),
+            Rotation3d_to_type(pose3d.r));
     }
 }
